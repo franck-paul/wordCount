ad187fa091d4bc7f896b37925bd6faf3b4335199
franck <carnet.franck.paul@gmail.com>
2021-10-23 12:45:10 +0200

    Add alternative repository

--- _define.php
+++ _define.php
@@ -10,20 +10,23 @@
  * @copyright Franck Paul carnet.franck.paul@gmail.com
  * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
  */
-
-if (!defined('DC_RC_PATH')) {return;}
+if (!defined('DC_RC_PATH')) {
+    return;
+}
 
 $this->registerModule(
-    "Word Count",                                                 // Name
-    "Counts characters, words and folios, reading time of entry", // Description
-    "Franck Paul",                                                // Author
+    'Word Count',                                                 // Name
+    'Counts characters, words and folios, reading time of entry', // Description
+    'Franck Paul',                                                // Author
     '0.6.1',                                                      // Version
     [
         'requires'    => [['core', '2.16']],                         // Dependencies
         'permissions' => 'usage,contentadmin',                       // Permissions
         'type'        => 'plugin',                                   // Type
-        'details'     => 'https://open-time.net/?q=wordCount',       // Details URL
-        'support'     => 'https://github.com/franck-paul/wordCount', // Support URL
-        'settings'    => []
+        'settings'    => [],
+
+        'details'    => 'https://open-time.net/?q=wordCount',        // Details URL
+        'support'    => 'https://github.com/franck-paul/wordCount',  // Support URL
+        'repository' => 'https://raw.githubusercontent.com/franck-paul/wordCount/main/dcstore.xml'
     ]
 );
