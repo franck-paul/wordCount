ed613e8420ce1e657196915a4c69c271af3eec2c
franck <carnet.franck.paul@gmail.com>
2023-10-24 06:54:03 +0200

    Use instanced item constants rather than interface ones

--- src/Backend.php
+++ src/Backend.php
@@ -15,7 +15,6 @@ declare(strict_types=1);
 namespace Dotclear\Plugin\wordCount;
 
 use Dotclear\App;
-use Dotclear\Core\Backend\Menus;
 use Dotclear\Core\Process;
 
 class Backend extends Process
@@ -35,7 +34,7 @@ class Backend extends Process
         }
 
         // Add menu item in blog menu
-        My::addBackendMenuItem(Menus::MENU_BLOG);
+        My::addBackendMenuItem(App::backend()->menus()::MENU_BLOG);
 
         $settings = My::settings();
         if ($settings->active && $settings->autorefresh) {
