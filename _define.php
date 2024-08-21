cc1e0489365f66518ec09b6201f4fca1a11021d7
franck <carnet.franck.paul@gmail.com>
2024-08-21 09:15:02 +0200

    New release 5.3

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'Word Count',
     'Counts characters, words and folios, reading time of entry',
     'Franck Paul',
-    '5.2',
+    '5.3',
     [
         'requires'    => [['core', '2.28']],
         'permissions' => 'My',
